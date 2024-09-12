739a5a10bbc496e19d0744c676396a081e95d08d
AntonSmishko <94136660+AntonSmishko@users.noreply.github.com>
2024-09-12 19:59:07 +0000

    Deploying to gh-pages from @ AntonSmishko/goit-js-hw-09@b14ee989a7b5d5dfa222c070c25e532785ef58ea 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/styles-ar1oFD8V.js";console.log("Gallery");
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-ar1oFD8V.js";console.log("Form");
+//# sourceMappingURL=2-form.js.map
