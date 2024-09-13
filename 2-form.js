468355b980964292caf94611ad549a9b4669d959
AntonSmishko <94136660+AntonSmishko@users.noreply.github.com>
2024-09-13 08:50:46 +0000

    Deploying to gh-pages from @ AntonSmishko/goit-js-hw-09@0daa1f4a06ad5cee51ad34c660b9aaf18a6dd389 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/styles-ar1oFD8V.js";console.log("Form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Form");
 //# sourceMappingURL=2-form.js.map
